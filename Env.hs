753b930493ecde04d60ac18bc4cf890a8d29976b
Timat-Piderma <mttpecc@gmail.com>
2024-11-14 17:33:03 +0100

    Added btype attribute and functions getVarType, typeToString and sup

--- Env.hs
+++ Env.hs
@@ -1,25 +1,29 @@
 module Env where
 import Data.Map as Map
 
+import AbsGram as Abs
+import GHC.Exts.Heap (StgInfoTable(entry))
+
 type EnvT = Map.Map String EnvEntity
 
 data EnvEntity = Variable {
-    varName :: String,
-    varPos :: (Int, Int)
+    name :: String,
+    pos :: (Int, Int),
+    btype :: BasicType
 }
     deriving (Show, Read)
 
 emptyEnv :: EnvT
 emptyEnv = Map.empty
 
-makeVar :: String -> (Int, Int) -> EnvEntity
-makeVar varName varPos = Variable varName varPos
+makeVar :: String -> (Int, Int) -> BasicType -> EnvEntity
+makeVar varName varPos varType = Variable varName varPos varType
 
 -- inserts only if not already in the environment
-insertVar :: String -> (Int, Int) -> EnvT -> EnvT
-insertVar varName varPos env = if containsVar varName env
+insertVar :: String -> (Int, Int) -> BasicType -> EnvT -> EnvT
+insertVar varName varPos varType env = if containsVar varName env
     then env
-    else Map.insert varName (makeVar varName varPos) env 
+    else Map.insert varName (makeVar varName varPos varType) env 
 
 containsVar :: String -> EnvT -> Bool
 containsVar varName env = 
@@ -29,5 +33,21 @@ containsVar varName env =
 
 getVarPos :: String -> EnvT -> (Int, Int)
 getVarPos varName env = case Map.lookup varName env of
-    Just entry -> varPos entry
+    Just entry  -> pos entry
     Nothing     -> (0,0)
+
+getVarType :: String -> EnvT -> BasicType
+getVarType varName env = case Map.lookup varName env of
+    Just entry  -> btype entry
+    Nothing     -> ERROR
+
+typeToString :: BasicType -> String
+typeToString ERROR = "Error"
+typeToString INT = "Integer"
+typeToString FLOAT = "Double"
+
+sup :: BasicType -> BasicType -> BasicType
+sup INT INT     = INT
+sup FLOAT INT   = FLOAT
+sup INT FLOAT   = FLOAT
+sup _ _         = ERROR
