fc9e70bc7929f5115ee612a9e01fd17ab3b539b4
Timat-Piderma <mttpecc@gmail.com>
2024-11-15 16:52:23 +0100

    Type functions moved to TypeSystem.hs

--- Env.hs
+++ Env.hs
@@ -1,26 +1,28 @@
 module Env where
 import Data.Map as Map
 
+import TypeSystem as TS 
 import AbsGram as Abs
 import GHC.Exts.Heap (StgInfoTable(entry))
+import Distribution.Compat.Lens (_1)
 
 type EnvT = Map.Map String EnvEntity
 
 data EnvEntity = Variable {
     name :: String,
     pos :: (Int, Int),
-    btype :: BasicType
+    btype :: Type
 }
     deriving (Show, Read)
 
 emptyEnv :: EnvT
 emptyEnv = Map.empty
 
-makeVar :: String -> (Int, Int) -> BasicType -> EnvEntity
+makeVar :: String -> (Int, Int) -> Type -> EnvEntity
 makeVar varName varPos varType = Variable varName varPos varType
 
 -- inserts only if not already in the environment
-insertVar :: String -> (Int, Int) -> BasicType -> EnvT -> EnvT
+insertVar :: String -> (Int, Int) -> Type -> EnvT -> EnvT
 insertVar varName varPos varType env = if containsVar varName env
     then env
     else Map.insert varName (makeVar varName varPos varType) env 
@@ -36,18 +38,7 @@ getVarPos varName env = case Map.lookup varName env of
     Just entry  -> pos entry
     Nothing     -> (0,0)
 
-getVarType :: String -> EnvT -> BasicType
+getVarType :: String -> EnvT -> Type
 getVarType varName env = case Map.lookup varName env of
     Just entry  -> btype entry
-    Nothing     -> ERROR
-
-typeToString :: BasicType -> String
-typeToString ERROR = "Error"
-typeToString INT = "Integer"
-typeToString FLOAT = "Double"
-
-sup :: BasicType -> BasicType -> BasicType
-sup INT INT     = INT
-sup FLOAT INT   = FLOAT
-sup INT FLOAT   = FLOAT
-sup _ _         = ERROR
+    Nothing     -> Base ERROR
